0714a4f4a65bb9c94fbe6801f48378c51dfe1bdd
BorjaEst <boressan@outlook.com>
2020-04-04 12:54:12 +0200

    Population parameters 8society) reviewed

--- include/society.hrl
+++ include/society.hrl
@@ -1,35 +1,32 @@
 %%%-------------------------------------------------------------------
 %%% @author borja
-%%% @copyright (C) 2018, <COMPANY>
 %%% @doc
 %%%
 %%% @end
-%%% Created : 16. Sep 2018 20:14
 %%%-------------------------------------------------------------------
 
--define(EV_POOL, ev_pool).
+-define(EV_POOL, society_pool).
 -define(EV_POOL_OPTIONS, [
-    named_table,        % The name of the table must be global for now
-    % public,             % As any process should be able to start a nn
-
+    named_table,        % The name of the table must be global
     set,                % The pool must be a set (no repeated values)
     {keypos, #population.id}    % The key of the record must be the id
 ]).
 
 % A population is a group of agents which run a (or in a) simulation..
 -record(population, {
-    id             :: ruler:id(),
-    supervisor     :: pid(),
-    ruler       :: pid(),
-    score_table    :: ets:tid(),
-    limit      = 5 :: integer(), % Maximum number of agents running at the same time
-    minimum    = 4 :: integer(), % Minimum number of agents before trigger evolutionary algorithm
-    current    = 0 :: integer(),
-    run_time   = infinity :: integer(), % Milliseconds of running time before pausing the population
-    run_agents = infinity :: integer(), % Number of Agents of tested before pausing the population
-    run_score  = infinity :: float(), % Score target before pausing the population
-    evo_alg_f  = fun evolutionary_algorithm:static_on_limit/2 :: function(),
-    sel_alg_f  = fun selection_algorithm:statistic_top_3/2 :: function()}).
+    id               :: ruler:id(), % Population identification
+    supervisor       :: pid(),      % Population supervisor
+    ruler            :: pid(),      % Population ruler
+    score_pool       :: ets:tid(),  % Id of the score pool
+    size       = 0   :: integer(),  % Population size
+    run_time   = 0   :: integer(),  % Time population has run
+    generation = 0   :: integer(),  % Number of run agents
+    best_score = 0.0 :: float()     % Max achieved score
+}).
+
+
+
+
 
 
 
