8c7f084e60d148e893c930bc956d0ca7f09530ec
BorjaEst <boressan@outlook.com>
2020-04-05 18:51:14 +0200

    record functions for ruler and agents implemented

--- src/demography.erl
+++ src/demography.erl
@@ -23,12 +23,12 @@
 -define(REFERENCE(Id), element(2,element(1,Id))).
 
 -record(ruler, {
-    id = ?NEW_RULER_ID :: ruler:id(),
-    size       = 5     :: integer(),      % Aagents per population
-    run_time   = infinity :: integer(),   % Time(ms) before end
-    run_agents = infinity :: integer(),   % Tested agents before end
-    run_score  = infinity :: float(),     % Score target before end
-    selection  = top3 :: selection:func() % Selection for agents pool
+    id = ?NEW_RULER_ID :: ruler:id(),      % Ruler/Population id
+    max_size    = 5     :: integer(),      % Population max agents
+    stop_time   = infinity :: integer(),   % Time(ms) before end
+    generations = infinity :: integer(),   % Tested agents before end
+    target      = infinity :: float(),     % Score target before end
+    selection = top3 :: selection:func()  % Selection for agents pool
 }).
 -type ruler() :: #ruler{}.
 
@@ -36,6 +36,7 @@
     id = ?NEW_AGENT_ID :: agent:id(),
     function    :: function(), % Function to perform
     arguments   :: term(),     % Arguments of the function
+    mutation    :: function(), % Mutation to modify the arguments
     father      :: agent:id()  % Id of the agent one generation back
 }). 
 -type agent() :: #agent{}.
@@ -100,15 +101,31 @@ edit(Agent, Properties) when is_record(Agent, agent) ->
 edit(Ruler, Properties) when is_record(Ruler, ruler) ->
     edit_ruler(Ruler, maps:to_list(Properties)).
 
-edit_agent(Agent, [{id,              Value} | Options]) ->
-   edit_agent(Agent#agent{id = Value}, Options);
-%%% TODO: To define
+edit_agent(Agent, [{id,        Value} | Options]) -> 
+    edit_agent(Agent#agent{       id = Value}, Options);
+edit_agent(Agent, [{function,  Value} | Options]) -> 
+    edit_agent(Agent#agent{ function = Value}, Options);
+edit_agent(Agent, [{arguments, Value} | Options]) -> 
+    edit_agent(Agent#agent{arguments = Value}, Options);
+edit_agent(Agent, [{mutation,  Value} | Options]) -> 
+    edit_agent(Agent#agent{ mutation = Value}, Options);
+edit_agent(Agent, [{father,    Value} | Options]) -> 
+    edit_agent(Agent#agent{   father = Value}, Options);
 edit_agent(Agent, []) -> 
     Agent.
 
 edit_ruler(Ruler, [{id,          Value} | Options]) -> 
-    edit_ruler(Ruler#ruler{id = Value}, Options);
-%%% TODO: To define
+    edit_ruler(Ruler#ruler{         id = Value}, Options); 
+edit_ruler(Ruler, [{max_size,    Value} | Options]) -> 
+    edit_ruler(Ruler#ruler{   max_size = Value}, Options); 
+edit_ruler(Ruler, [{stop_time,   Value} | Options]) -> 
+    edit_ruler(Ruler#ruler{  stop_time = Value}, Options); 
+edit_ruler(Ruler, [{generations, Value} | Options]) -> 
+    edit_ruler(Ruler#ruler{generations = Value}, Options); 
+edit_ruler(Ruler, [{target,      Value} | Options]) -> 
+    edit_ruler(Ruler#ruler{     target = Value}, Options); 
+edit_ruler(Ruler, [{selection,   Value} | Options]) -> 
+    edit_ruler(Ruler#ruler{  selection = Value}, Options); 
 edit_ruler(Ruler, []) ->
     Ruler.
 
@@ -135,6 +152,13 @@ function(Agent) when is_record(Agent, agent) -> Agent#agent.function.
 arguments(Agent) when is_record(Agent,agent) -> Agent#agent.arguments.
 
 %%--------------------------------------------------------------------
+%% @doc Returns the agent mutation function.
+%% @end
+%%--------------------------------------------------------------------
+-spec mutation(Agent :: agent()) -> term().
+mutation(Agent) when is_record(Agent,agent) -> Agent#agent.mutation.
+
+%%--------------------------------------------------------------------
 %% @doc Returns the agent father.
 %% @end
 %%--------------------------------------------------------------------
@@ -152,6 +176,42 @@ tree(Agent) when is_record(Agent, agent) ->
 tree({_, agent} = Agent_Id) ->
     tree(ndb:read(Agent_Id));
 tree(_Other) -> [].
+
+%%--------------------------------------------------------------------
+%% @doc Returns the population maximum size allowed by the ruler.
+%% @end
+%%--------------------------------------------------------------------
+-spec max_sixe(Ruler :: ruler()) -> Max_Size :: integer().
+max_sixe(Ruler) when is_record(Ruler, ruler) -> Ruler#ruler.max_size.
+
+%%--------------------------------------------------------------------
+%% @doc Returns the maximum running time allowed in milliseconds.
+%% @end
+%%--------------------------------------------------------------------
+-spec stop_time(Ruler :: ruler()) -> StopTime :: integer().
+stop_time(Ruler) when is_record(Ruler, ruler) -> Ruler#ruler.stop_time.
+
+%%--------------------------------------------------------------------
+%% @doc Returns the ruler planed maximum generations.
+%% @end
+%%--------------------------------------------------------------------
+-spec generations(Ruler :: ruler()) -> generations.
+generations(Ruler) when is_record(Ruler, ruler) -> 
+    Ruler#ruler.generations.
+
+%%--------------------------------------------------------------------
+%% @doc Returns the ruler score target.
+%% @end
+%%--------------------------------------------------------------------
+-spec target(Ruler :: ruler()) -> ScoreTarget :: float().
+target(Ruler) when is_record(Ruler, ruler) -> Ruler#ruler.target.
+
+%%--------------------------------------------------------------------
+%% @doc Returns the ruler selection.
+%% @end
+%%--------------------------------------------------------------------
+-spec selection(Ruler :: ruler()) -> selection:func().
+selection(Ruler) when is_record(Ruler, ruler) -> Ruler#ruler.selection.
 
 
 %%--------------------------------------------------------------------
